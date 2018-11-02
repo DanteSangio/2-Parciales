089b59521084c79540d9dd10bc5407cab0de0050
Fede <Fede@192.168.2.19>
2018-11-02 00:30:00 -0300

    Enunciado y comentarios

--- Example/src/FRTOS.c
+++ Example/src/FRTOS.c
@@ -1,29 +1,55 @@
 /*
-===============================================================================
-===============================================================================
- Name        : FRTOS.c
- Author      : $(author)
- Version     :
- Copyright   : $(copyright)
- Description : main definition
-===============================================================================
+TECNICAS DIGITALES II - 2do PARCIAL TEMA 84 - Parte practica:
+
+El sistema estará encargado de monitorear una vez por segundo a 04 (cuatro) sensores de temperatura, ubicados en
+distintos puntos de una cámara frigorífica, y, en función de los valores medidos, se encargará del accionamiento de 04
+(cuatro) equipos de acondicionamiento de temperatura.
+Siendo que los sensores se encontrarán en áreas susceptibles a inducciones de ruido, se utilizarán lazos 4-20 mA para
+interconectarlos con la placa central.
+Las especificaciones técnicas de los sensores son:
+			Temperatura[°C] 	Corriente
+			-40°C 				04 mA
+			 10°C 				20 mA
+Se solicita:
+a) Desarrollar toda la electrónica necesaria, incluido el amplificador de transresistencia para convertir los 4-20mA
+a 1 - 2,5 Volts de forma que pueda ser leído por el conversor ADC del microcontrolador. Realice el/los diagrama/s
+esquemático/s correspondiente/s.
+b) Deberá desarrollar el sistema de control del tipo ON/OFF de la cámara frigorífica. La variable a controlar será la
+Temperatura. Para garantizar el correcto mantenimiento de los alimentos, la temperatura deberá encontrarse en todo
+momento entre -25°C y -10°C en los 04 sectores monitoreados de la cámara. Para esto, deberá:
+• Si Tx < -25°C, apague acondicionador de temperatura correspondiente (Acx)
+• Si Tx > -10°C, encienda acondicionador de temperatura correspondiente (Acx)
+c) Si en algún momento, la temperatura supera los -5 °C en cualquiera de los 04 (cuatro) sensores de
+temperatura, el sistema deberá accionar una alarma sonora de 220V por 30 segundos.
+d) Si en algún momento, la temperatura supera los 0 °C en cualquiera de los 04 (cuatro) sensores de temperatura.
+El sistema deberá guardar en la posición 0x10 de una memoria EEPROM externa (conectada al módulo I2C1 bajo
+direccionamiento físico 0x00) el valor 0xAA y apagar el sistema siendo que los alimentos deberán ser descartados. Este
+parámetro será auditado ante algún control bromatológico.
+Reglas de diseño:
+• Se encuentra prohibida la utilización de variables globales.
+• El sistema deberá ser desarrollado íntegramente bajo FreeRTOS utilizando los mecanismos de sincronización
+provistos por el sistema operativo.
+• Las temporizaciones deberán generarse con los Timers provistos por el microcontrolador.
+• El acceso al hardware deberá ser desarrollado utilizando LPCOpen.
 */
+
+
+//*********************************************************************************************************************
+//INCLUDES
+//*********************************************************************************************************************
+
 #include "chip.h"
 #include "FreeRTOS.h"
 #include "task.h"
 #include "semphr.h"
 #include "queue.h"
-
-SemaphoreHandle_t Semaforo_Sonoro;
-SemaphoreHandle_t Semaforo_2;
-
-QueueHandle_t	  ColaADCZona1,ColaADCZona2,ColaADCZona3,ColaADCZona4;
-
 #include <cr_section_macros.h>
 
-// TODO: insert other include files here
 
-// TODO: insert other definitions and declarations here
+//*********************************************************************************************************************
+//DEFINES
+//*********************************************************************************************************************
+
 #define PORT(x) 	((uint8_t) x)
 #define PIN(x)		((uint8_t) x)
 
@@ -35,7 +61,6 @@ QueueHandle_t	  ColaADCZona1,ColaADCZona2,ColaADCZona3,ColaADCZona4;
 #define SEN_3		ADC_CH6
 #define SEN_4		ADC_CH7
 
-
 //I2C
 #define PUERTO_0  	 		0
 #define I2C1				1
@@ -43,8 +68,8 @@ QueueHandle_t	  ColaADCZona1,ColaADCZona2,ColaADCZona3,ColaADCZona4;
 #define PIN_SCL1  	   	   20
 #define NEITHER				2
 #define I2C_FUNC     		3
-#define SLAVE_ADDRESS		0x50 // 1010 bits x default de la memorio 0000 los primeros 3 son el direccionamiento fisico
-#define W_ADDRESS			0x0010 // posicion donde voy a escribir
+#define SLAVE_ADDRESS		0x50 	// 1010 bits x default de la memorio 0000 los primeros 3 son el direccionamiento fisico
+#define W_ADDRESS			0x0010 	// posicion donde voy a escribir
 #define DATO				0xAA
 
 //Placa Infotronic
@@ -66,14 +91,22 @@ QueueHandle_t	  ColaADCZona1,ColaADCZona2,ColaADCZona3,ColaADCZona4;
 #define SONORA		PORT(0),PIN(8)
 #define PUL_EMER	PORT(0),PIN(9)
 
-
-#define LIMITE_INF_TEMP	1800//1.45V es -25°C que son 1800 cuentas
+#define LIMITE_INF_TEMP	1800	//1.45V es -25°C que son 1800 cuentas
 #define LIMITE_SUP_TEMP	2358	//1.9V  es -10°C que son 2358 cuentas
 #define LIMITE_SON_TEMP	2544	//2.05V es -5°C que son 2544 cuentas
 #define LIMITE_MAX_TEMP	2730	//2.2V  es 0°C que son 2730 cuentas
 
+//*********************************************************************************************************************
+//DECLARACIONES
+//*********************************************************************************************************************
+
+SemaphoreHandle_t Semaforo_Sonoro;
+SemaphoreHandle_t Semaforo_2;
+
+QueueHandle_t	  ColaADCZona1,ColaADCZona2,ColaADCZona3,ColaADCZona4;
 
 static ADC_CLOCK_SETUP_T ADCSetup;
+
 
 //*********************************************************************************************************************
 
@@ -81,6 +114,7 @@ void I2C1_IRQHandler(void)
 {
 	Chip_I2C_MasterStateHandler (I2C1);
 }
+
 
 //*********************************************************************************************************************
 
@@ -112,8 +146,8 @@ void ADC_IRQHandler(void)
 		xQueueSendToBackFromISR(ColaADCZona4, &dataADC, &testigo);
 		portYIELD_FROM_ISR(testigo);
 	}
-
 }
+
 
 //*********************************************************************************************************************
 
@@ -122,13 +156,11 @@ static void ADC_Config(void *pvParameters)
 	Chip_IOCON_PinMux(LPC_IOCON, 1, 31, IOCON_MODE_INACT, IOCON_FUNC3); // Entrada analogica 0 (Infotronik) ch5
 	Chip_IOCON_PinMux(LPC_IOCON, 1, 30, IOCON_MODE_INACT, IOCON_FUNC3); // Entrada analogica 0 (Infotronik) ch5
 
-
 	Chip_ADC_Init(LPC_ADC, &ADCSetup);
 	Chip_ADC_EnableChannel(LPC_ADC, SEN_1, ENABLE);
 	Chip_ADC_EnableChannel(LPC_ADC, SEN_2, ENABLE);
 	Chip_ADC_EnableChannel(LPC_ADC, SEN_3, ENABLE);
 	Chip_ADC_EnableChannel(LPC_ADC, SEN_4, ENABLE);
-
 
 	Chip_ADC_SetSampleRate(LPC_ADC, &ADCSetup, 50000);
 	Chip_ADC_Int_SetChannelCmd(LPC_ADC, SEN_1, ENABLE);
@@ -143,7 +175,6 @@ static void ADC_Config(void *pvParameters)
 	Chip_ADC_SetStartMode (LPC_ADC, ADC_START_NOW, ADC_TRIGGERMODE_RISING);
 
 	vTaskDelete(NULL);
-
 }
 
 //*********************************************************************************************************************
@@ -162,7 +193,6 @@ static void I2C_Config(void *pvParameters)
 	NVIC_EnableIRQ(I2C1_IRQn);
 
 	vTaskDelete(NULL);
-
 }
 
 //*********************************************************************************************************************
@@ -171,11 +201,12 @@ static void taskAnalisis(void *pvParameters)
 {
 	uint16_t datoTemp[4],i=0;
 	unsigned char Datos_Tx [] = { (W_ADDRESS & 0xFF00) >> 8 , W_ADDRESS & 0x00FF, DATO};
+
 	while(1)
 	{
-
 		Chip_ADC_SetStartMode (LPC_ADC, ADC_START_NOW, ADC_TRIGGERMODE_RISING);
-		//me aseguro que los datos ya esten en las colas
+
+		//Me aseguro que los datos ya esten en las colas:
 		xQueueReceive( ColaADCZona1, &datoTemp[0], portMAX_DELAY );
 		xQueueReceive( ColaADCZona2, &datoTemp[1], portMAX_DELAY );
 		xQueueReceive( ColaADCZona3, &datoTemp[2], portMAX_DELAY );
@@ -183,68 +214,84 @@ static void taskAnalisis(void *pvParameters)
 
 		for(i=0;i<4;i++)
 		{
-			if(datoTemp[i]>LIMITE_SUP_TEMP)
+			if(datoTemp[i] > LIMITE_SUP_TEMP)
 			{
+				//Se debe encender el aire correspondiente  a la zona i:
 				switch(i)
 				{
-				  case 1:
+					case 1:
 						Chip_GPIO_SetPinOutHigh (LPC_GPIO , AIRE1);
-				  case 2:
+					break;
+					case 2:
 						Chip_GPIO_SetPinOutHigh (LPC_GPIO , AIRE2);
-				  case 3:
+					break;
+					case 3:
 						Chip_GPIO_SetPinOutHigh (LPC_GPIO , AIRE3);
-				  case 4:
+					break;
+					case 4:
 						Chip_GPIO_SetPinOutHigh (LPC_GPIO , AIRE4);
-				}//encender el aire correspondiente  a la zona i
+					break;
+				}
 			}
 
 			if(datoTemp[i] < LIMITE_INF_TEMP)
 			{
+				//Se debe apagar el aire correspondiente  a la zona i:
 				switch(i)
 				{
-				  case 1:
+					case 1:
 						Chip_GPIO_SetPinOutLow (LPC_GPIO , AIRE1);
-				  case 2:
-					  Chip_GPIO_SetPinOutLow (LPC_GPIO , AIRE2);
-				  case 3:
-					  Chip_GPIO_SetPinOutLow (LPC_GPIO , AIRE3);
-				  case 4:
-					  Chip_GPIO_SetPinOutLow (LPC_GPIO , AIRE4);
-				}//encender el aire correspondiente  a la zona i//apagar el aire correspondiente  a la zona i
-
+					break;
+					case 2:
+						Chip_GPIO_SetPinOutLow (LPC_GPIO , AIRE2);
+					break;
+					case 3:
+						Chip_GPIO_SetPinOutLow (LPC_GPIO , AIRE3);
+					break;
+					case 4:
+						Chip_GPIO_SetPinOutLow (LPC_GPIO , AIRE4);
+					break;
+				}
 			}
+
 			if(datoTemp[i] > LIMITE_SON_TEMP)
 			{
-				xSemaphoreGive(Semaforo_Sonoro );
+				//Se debe activar la alarma sonora:
+				xSemaphoreGive(Semaforo_Sonoro);
 			}
+
 			if(datoTemp[i] > LIMITE_MAX_TEMP)
 			{
+				//Se debe apagar los aires y guardar en memoria EEPROM externa:
 				Chip_GPIO_SetPinOutLow (LPC_GPIO , AIRE1);
 				Chip_GPIO_SetPinOutLow (LPC_GPIO , AIRE2);
 				Chip_GPIO_SetPinOutLow (LPC_GPIO , AIRE3);
-				Chip_GPIO_SetPinOutLow (LPC_GPIO , AIRE4); //apagar todos los aires
+				Chip_GPIO_SetPinOutLow (LPC_GPIO , AIRE4);
 				NVIC_ClearPendingIRQ(ADC_IRQn);
 				NVIC_DisableIRQ(ADC_IRQn);
-			    Chip_I2C_MasterSend (I2C1, SLAVE_ADDRESS, Datos_Tx,3); //Selecciono el lugar y escribo el dato.
+			    Chip_I2C_MasterSend (I2C1, SLAVE_ADDRESS, Datos_Tx,3); //Selecciono el lugar y escribo el dato
 			}
 		}
 	}
 }
 
+
 //*********************************************************************************************************************
-/* ENTRADAS */
+
 static void vTaskSonora(void *pvParameters)
 {
-	while (1)
+	while(1)
 	{
 		xSemaphoreTake(Semaforo_Sonoro , portMAX_DELAY );
 		Chip_GPIO_SetPinOutHigh (LPC_GPIO , SONORA);
-		vTaskDelay( 30000 / portTICK_PERIOD_MS );//Delay de 30 seg
+		vTaskDelay( 30000 / portTICK_PERIOD_MS );		//Delay de 30 seg
 		Chip_GPIO_SetPinOutLow (LPC_GPIO , SONORA);
-
 	}
 }
+
+
 //*********************************************************************************************************************
+
 void uC_StartUp (void)
 {
 	Chip_GPIO_Init (LPC_GPIO);
@@ -290,6 +337,7 @@ void uC_StartUp (void)
 int main(void)
 {
 	uC_StartUp ();
+
 	SystemCoreClockUpdate();
 
 	vSemaphoreCreateBinary(Semaforo_Sonoro);
@@ -299,7 +347,6 @@ int main(void)
 	ColaADCZona2 = xQueueCreate (1, sizeof(uint16_t));
 	ColaADCZona3 = xQueueCreate (1, sizeof(uint16_t));
 	ColaADCZona4 = xQueueCreate (1, sizeof(uint16_t));
-
 
 	xSemaphoreTake(Semaforo_Sonoro , portMAX_DELAY );
 
@@ -323,6 +370,5 @@ int main(void)
 	vTaskStartScheduler();
 
 	/* Nunca debería arribar aquí */
-
     return 0;
 }
